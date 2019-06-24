7d82309dddcd951ebeaceaa4ce7ceea7c8d3d3c7
Louis Gesbert <louis.gesbert@ocamlpro.com>
2019-06-24 17:38:42 +0200

    Add a 'switch invariant' in switch-config that is enforced by the solver

--- src/format/opamFile.ml
+++ src/format/opamFile.ml
@@ -1494,6 +1494,7 @@ module Switch_configSyntax = struct
     opam_root: dirname option;
     wrappers: Wrappers.t;
     env: env_update list;
+    invariant: OpamFormula.t;
   }
 
   let empty = {
@@ -1505,6 +1506,7 @@ module Switch_configSyntax = struct
     opam_root = None;
     wrappers = Wrappers.empty;
     env = [];
+    invariant = OpamFormula.Empty;
   }
 
   let sections = [
@@ -1541,6 +1543,10 @@ module Switch_configSyntax = struct
     "setenv", Pp.ppacc
       (fun env t -> {t with env}) (fun t -> t.env)
       (Pp.V.map_list ~depth:2 Pp.V.env_binding);
+    "invariant", Pp.ppacc
+      (fun invariant t -> {t with invariant}) (fun t -> t.invariant)
+      (Pp.V.package_formula `Conj Pp.V.(constraints version));
+
   ] @
     List.map
       (fun (fld, ppacc) ->
